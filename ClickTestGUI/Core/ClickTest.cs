fd40ed545eded1a44b6d1db8edcd5ad0266980a4
N3ls0nQ <quentin.hoehne062@gmail.com>
2021-09-01 03:37:11 +0200

    Add project files.

---
+++ ClickTestGUI/Core/ClickTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace ClickTestGUI.Core
+{
+    public class ClickTest
+    {
+        public int Score { get; set; }
+        public int LastScore { get; set; }
+        public double Counts;
+        public int chosenTime = 5;
+        public bool isTimerRunning;
+        public DispatcherTimer timer {get; set;}
+       
+
+
+        public ClickTest()
+        {
+            timer = new DispatcherTimer(DispatcherPriority.Send);
+            Debug.Print("ClickTest object initiated!");
+        }
+
+        public void startTimer(object _buttonClicked)
+        {
+            bool buttonClicked = (bool)_buttonClicked;
+
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 0,1);
+
+            if (buttonClicked && !isTimerRunning)
+            {
+                isTimerRunning = true;
+                timer.Start();
+                Debug.Print("Timer has started!");
+
+            }
+
+        }
+
+        public void Restart()
+        {
+            Debug.Print("Restarting...");
+        }
+
+        public void EndTest()
+        {
+            timer.Stop();
+            Counts = 0;
+            LastScore = Score;
+            Score = 0;
+
+            Debug.Print("Test ended!");
+            //throw new NotImplementedException();
+        }
+
+        public void timer_Tick(object sender, EventArgs e)
+        {
+            Counts ++;
+            Debug.Print(chosenTime - Counts + " Seconds");
+
+            if (Counts == chosenTime)
+            {
+                EndTest();
+            }
+        }
+    }
+}
---
+++ ClickTestGUI/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿
+using ClickTestGUI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClickTestGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ClickTest clickTest = new ClickTest();
+        bool buttonClicked = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Close();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            clickTest.Score++;
+            buttonClicked = true;
+
+            if (clickTest.Score == 1)
+            {
+                clickTest.startTimer(buttonClicked);
+            }
+
+            clickTest.timer.Tick += Timer_Tick;
+            TestButton.Content = "Clicks: " + clickTest.Score;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            TimeLable.Content = "Time left: " + (clickTest.chosenTime - clickTest.Counts) + " Seconds";
+
+            if (clickTest.Counts + clickTest.chosenTime == clickTest.chosenTime)
+            {
+                TimeLable.Content = "Test ended!";
+                TestButton.IsEnabled = false;
+            }
+        }
+
+        private void Image_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            if (!clickTest.timer.IsEnabled)
+            {
+                clickTest.Restart();
+            }
+            
+        }
+    }
+}
